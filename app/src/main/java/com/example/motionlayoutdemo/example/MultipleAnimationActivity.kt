8dc6c986b1884ca69fa1d28780c340423e44d1d8
蓝伟煌 <1078049810@qq.com>
2022-04-06 15:53:54 +0800

    拖动动画案例

---
+++ app/src/main/java/com/example/motionlayoutdemo/example/MultipleAnimationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.motionlayoutdemo.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.motionlayoutdemo.R
+
+class MultipleAnimationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiple_animation)
+
+
+    }
+}
